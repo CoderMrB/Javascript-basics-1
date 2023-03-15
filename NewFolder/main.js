6cee779d4fcb124822fa761cb8bdc57a05aaf253
Chris Branson <chrisbranson@live.co.uk>
2023-03-15 20:34:16 +0000

    Added conditional statements to JS

--- NewFolder/main.js
+++ NewFolder/main.js
@@ -1,1 +1,27 @@
 console.log ("HELLO WORLD!");
+
+let myFavNumber = 80
+
+if(myFavNumber === 5){
+    console.log ("My favourite number is 5 too!");
+}
+
+let my_variable = prompt(`What's your name?`)
+
+if (my_variable === `Chris`){
+    console.log("That's my name too!")
+} else {
+    console.log(my_variable + "?! That's a silly name...")
+}
+
+let weatherType = `raining`
+
+if (weatherType === `raining`){
+    console.log(`Take an umbrella!`)
+} else{
+    if (weatherType === `sunny`){
+        console.log (`Don't forget suncream!`)
+        }else{
+        console.log (`Must be a normal, grey, non-descript day!`)
+}
+}
