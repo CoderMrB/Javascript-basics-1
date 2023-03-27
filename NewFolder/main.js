39fd86a4bb090d0a36d4bc7b5a6f05a75f238515
Chris Branson <chrisbranson@live.co.uk>
2023-03-27 20:32:05 +0100

    Practised if/else, prompts, alerts

--- NewFolder/main.js
+++ NewFolder/main.js
@@ -1,13 +1,27 @@
 console.log ("HELLO WORLD!");
 
+let myFaveNumber = prompt("What's you favourite number?")
+if (myFaveNumber == 5){
+    alert("My favourite number is 5 too!")
+}else{
+    alert("You're my number 1.")
+}
+
+
 let userName = prompt("What's your name?")
 
 if (userName === "Ben") {
-    
+    alert("That's my name too!")
+}else{
+    alert(`${userName}?! That's a silly name...`)
 }
 
-let myFaveNumber = 80
+let weatherType = prompt("What's the weather like today?")
 
-if (myFaveNumber === 5){
-    console.log("My favourite number is 5 too!")
+if (weatherType === "raining"){
+    alert("Take an umbrella!")
+}else if (weatherType === "sunny"){
+    alert("Don't forget suncream!")
+}else{
+    alert("Must be a normal, grey, non-descript day!")
 }
